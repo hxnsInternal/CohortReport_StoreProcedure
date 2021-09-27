926c150317fcb4da95e46cf6f5ad1c790d466cb4
Hans Zamora Carrillo <40968248+hxnsInternal@users.noreply.github.com>
2021-09-27 02:10:49 -0500

    Update 02 - StoreProcedureCohortReport.sql

--- 02 - StoreProcedureCohortReport.sql
+++ 02 - StoreProcedureCohortReport.sql
@@ -15,7 +15,7 @@ begin
 	
 	discard temp;
 
-	drop table if exists tmp_baseCohort1;
+	drop table if exists tmp_baseCohort1; -->> Creating a ase table with the information of the login and transaction info.
 	create table tmp_baseCohort1 as
 	select unique_id client
 		,to_char(signup_at,'YYYYMM') suscription_mnth 
@@ -26,7 +26,7 @@ begin
 	where t.trx_at >= tu.signup_at 
 		order by 2,1;
 	
-	select count(1) into liCount
+	select count(1) into liCount		--> Getting the all rows of the table "tmp_baseCohort1"
 	from tmp_baseCohort1;
 	
 	RAISE NOTICE 'OK: tmp_baseCohort1 Created. Rows ->> %', liCount;
@@ -35,14 +35,14 @@ begin
 	from tmp_baseCohort1;
 
 	lsSql2 := 'create table tbl_cohort_report as '|| chr(10) || 
-			   'with head as(
+			   'with head as(		-->> getting quantitys of the data
 					select b.suscription_mnth 
 						,count(distinct(client)) quantity
 					from tmp_baseCohort1 b
 						group by b.suscription_mnth
 				),
-				body as (	
-					select *
+				body as (		-->> Using a table pivot funtion to pivot the information.
+					select *	
 					from crosstab('' 		
 					select suscription_mnth 
 							,mnth 
@@ -57,11 +57,11 @@ begin
 			 
 	lsSql := 'select distinct mnth from tmp_baseCohort1 order by 1';
 
-	-- Starting with the dynamic query to complete the all sentence .
+	-- Starting with the dynamic query to complete the all sentence, generating a DDL definition for the report table
 	
 	for lrRec in execute lsSql loop
 
-			if liIter != liCount then
+			if liIter != liCount then  --> It's just for not add a space line when is the last iteration
 			
 				lsSql2 := lsSql2 || ',mnth_'|| liIter ||' int' || chr(10);
 			else
@@ -84,7 +84,7 @@ begin
 				 
 	for lrRec in execute lsSql loop
 
-		if liIter != liCount then
+		if liIter != liCount then	--> It's just for not add a space line when is the last iteration
 			lsSql2 := lsSql2 || ',c.mnth_' || liIter || chr(10);
 		else
 			lsSql2 := lsSql2 || ',c.mnth_' || liIter;
@@ -121,13 +121,13 @@ begin
 	liIter := 1;
 
 	lsSql2 := lsSql2 || '
-						from basecohort
-							order by 1';
+				from basecohort
+					order by 1';
 
 	RAISE NOTICE 'Query: ->>> %', lsSql2;  --> Display the SQL Query for the CTAS report.	
 
-	drop table if exists tbl_cohort_report;
-	execute lsSql2;
+	drop table if exists tbl_cohort_report;		
+	execute lsSql2;		-->> Creating the new report table with the information of the report.
 	
 						
 end; $$
